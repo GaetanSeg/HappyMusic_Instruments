d14eb8d1f68b7c1a7e6be53a92be5cba2318f5f4
HappyMusic <supra3946@gmail.com>
2018-04-09 22:22:46 +0200

    test

--- application/views/article/article.php
+++ application/views/article/article.php
@@ -1,21 +1,25 @@
-
 <?php  if($articles):?>
-<ul class="thumbnails">
-      <li class="row">
-        <?php foreach ($articles as $a): $show = site_url('article/show/'.$a->article_id);?>
-          <div class="ccol-sm-6 col-md-4">
+  <ul class="thumbnails">
+    <li class="row">
+      <?php foreach ($articles as $a): $show = site_url('article/show/'.$a->article_id);?>
+          <div class="card" style="width: 18rem;">
             <a href="<?php echo $show;?>"><img src="<?php echo $this->picture_path.$a->image_name;?>" alt="<?php $a->title; ?>"width="156" height="156"></a>
-            <div class="caption">
-                <h3><?php echo $a->title;?></h3>
-                <h3><?php echo $a->categorie_name;?></h3>
-                <p><?php echo character_limiter($a->description,80);?></p>
-                <p>
-                  <a href="<?php echo $show; ?>" class="btn btn-outline-primary">détails</a>
-                  <a href="<?php echo $show; ?>" class="btn btn-outline-secondary"><?php  echo number_format($a->price_amount,2,',',' '); ?>€</a>
-                </p>
+
+            <div class="card-body">
+              <h5 class="card-title"><?php echo $a->title;?></h5>
+              <p class="card-text"><?php echo $a->categorie_name;?></p>
+              <hr>
+              <p><?php echo character_limiter($a->description,80);?></p>
+              <a href="<?php echo $show; ?>" class="btn btn-outline-primary">Voir plus...</a>
+              <a href="<?php echo $show; ?>" class="btn btn-outline-secondary"><?php  echo number_format($a->price_amount,2,',',' '); ?>€</a>
             </div>
-        </div>
-        <?php endforeach; ?>
-      </li>
-</ul>
+          </div>
+          &nbsp;
+          &nbsp;
+          &nbsp;
+          &nbsp;
+      <?php endforeach; ?>
+    </li>
+  </ul>
+
 <?php endif;?>
